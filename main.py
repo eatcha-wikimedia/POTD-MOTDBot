5830bdd000c6508abca8ca035a75310138cb8c0d
I'm User:Eatcha <51746381+eatcha-wikimedia@users.noreply.github.com>
2020-04-06 08:38:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ import pywikibot
 import re, datetime
 
 TODAY = datetime.datetime.utcnow()
-
 SITE = pywikibot.Site()
 
 def informatdate():
@@ -14,11 +13,10 @@ def get_motd_page_today():
 def get_potd_page_today():
     return 'Template:Potd/%s' % informatdate()
 
-def getfile(What=None,text):
+def getfile(text):
     return ("File:"+re.search(r"{{(?:\s*)[MmPp]otd(?:[_\s\-]|)[Ff]ilename(?:\s*)\|(?:1=|)(.*?)\|", text).group(1))
 
 def Notify(UserName,File,What=None):
-    
 
 def commit(old_text, new_text, page, summary):
     """Show diff and submit text to page."""
