a9aaea71fdc3009af6ee4928708efe156882f6b0
I'm User:Eatcha <51746381+eatcha-wikimedia@users.noreply.github.com>
2020-04-09 20:00:42 +0530

    Update copy-desc.py

--- copy-desc.py
+++ copy-desc.py
@@ -2,3 +2,17 @@
 import re
 import pywikibot
 from datetime import datetime
+
+
+def get_day_stuff(what,dateinformat):
+    if what is "MOTD":
+    if what is "POTD":
+
+def main():
+    
+
+if __name__ == "__main__":
+  try:
+    main()
+  finally:
+    pywikibot.stopme()
