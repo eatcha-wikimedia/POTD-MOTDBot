cab9c4be760ee31c170ee2297a80fcca01389928
I'm User:Eatcha <51746381+eatcha-wikimedia@users.noreply.github.com>
2020-04-19 19:53:04 +0530

    Update copy-desc.py

--- copy-desc.py
+++ copy-desc.py
@@ -2,8 +2,8 @@
 import re
 import pywikibot
 from datetime import datetime, timedelta
-# from googletrans import Translator
-# translator = Translator()
+#from googletrans import Translator
+#translator = Translator()
 
 
 SITE = pywikibot.Site()
@@ -11,23 +11,6 @@ SITE = pywikibot.Site()
 def informatdate(prev):
     """Current date in yyyy-mm-dd format."""
     return (datetime.utcnow()-timedelta(days=prev)).strftime('%Y-%m-%d')
-
-def commit(old_text, new_text, page, summary):
-    """Show diff and submit text to page."""
-    out("\nAbout to make changes at : '%s'" % page.title())
-    pywikibot.showDiff(old_text, new_text)
-    page.put(new_text, summary=summary, watchArticle=True, minorEdit=False)
-
-def out(text, newline=True, date=False, color=None):
-    """Just output some text to the consoloe or log."""
-    if color:
-        text = "\03{%s}%s\03{default}" % (color, text)
-    dstr = (
-        "%s: " % TODAY.strftime("%Y-%m-%d %H:%M:%S")
-        if date
-        else ""
-    )
-    pywikibot.stdout("%s%s" % (dstr, text), newline=newline)
 
 def get_page_name(what,dateinformat):
     page_name = None
@@ -76,6 +59,26 @@ def get_valid_langs(basepage):
             existant_lang_pages.append(lang_page_name)
     return existant_lang_pages
 
+def out(text, newline=True, date=False, color=None):
+    """Just output some text to the consoloe or log."""
+    if color:
+        text = "\03{%s}%s\03{default}" % (color, text)
+    dstr = (
+        "%s: " % datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+        if date
+        else ""
+    )
+    pywikibot.stdout(
+        "%s%s" % (dstr, text),
+        newline=newline,
+        )
+
+def commit(old_text, new_text, page, summary):
+    """Show diff and submit text to the wiki server."""
+    out("\nAbout to make changes at : '%s'" % page.title())
+    pywikibot.showDiff(old_text,new_text,)
+    page.put(new_text,summary=summary,watchArticle=True,minorEdit=False,)
+
 def add_to_file(filename,list_of_lang_templates,stuff,template_name):
     page = pywikibot.Page(
         SITE,
@@ -85,12 +88,11 @@ def add_to_file(filename,list_of_lang_templates,stuff,template_name):
     desc_text = re.search(r"\|[Dd]escription(?:\s*?)=(?:\s*?)([\s\S]*?)\n(?:\s*?)\|",old_text).group(1)
     text_to_append = ""
     for template in list_of_lang_templates:
-        text_to_append = "\n%s" % template
+        text_to_append = "%s\n%s" % (text_to_append,template)
     updated_desc = desc_text + text_to_append
     new_text = old_text.replace(desc_text,updated_desc)
     summary = "Adding descriptions from [[%s|%s]] template." % (template_name,stuff)
-    commit(old_text,new_text,page,summary)
-    #print(new_text)
+    commit(old_text, new_text, page, summary)
 
 def detectUnIdentifedlangs(text):
     regex = r"description(?:\s*)=(?:\s*)({{[\s\S]*?}})\n(?:\s*)\|"
@@ -99,7 +101,7 @@ def detectUnIdentifedlangs(text):
     except:
         text_to_remove = ""
     without_identfied_langs = re.sub(text_to_remove,"",text)
-    
+
 def checkIfTemplatePresent(langcode,text):
     regex = "{{(?:\s*)%s(?:\s*)\|" % langcode
 
@@ -107,11 +109,10 @@ def checkIfTemplatePresent(langcode,text):
         re.search(regex,text).group()
     except:
         return False
-
     return True
 
 def handle(stuff):
-    for num in range(5,5648):
+    for num in range(1,5648):
         dateinformat = informatdate(num) # how many days before
         page_name = get_page_name(stuff,dateinformat)
         page = pywikibot.Page(
@@ -121,7 +122,7 @@ def handle(stuff):
             filename = "File:"+re.search(r"[Ff]ilename\|(?:1=|)(.*?)\|", page.get()).group(1)
         except Exception as e:
             print(e)
-        out("now processing - " + stuff + " - "+ filename,color="green")
+        out("now processing - " + stuff + " - " + filename,color="green")
         if page.exists():
             langs_array = get_valid_langs(page_name)
             lang_add_list = []
