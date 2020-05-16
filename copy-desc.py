7b969e2382301cccb211ffc7b15b3926e2b81916
User:Eatcha <51746381+eatcha-wikimedia@users.noreply.github.com>
2020-05-16 08:07:59 +0530

    Update copy-desc.py

--- copy-desc.py
+++ copy-desc.py
@@ -168,7 +168,7 @@ def main():
         "MOTD",
         ]
 
-    for num in range(-10,+5):
+    for num in range(-3,+10):
         for stuff in day_pages:
             try:
                 handle(stuff,num)
